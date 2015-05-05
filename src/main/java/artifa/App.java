c60de67fa6055a8f929ff811dce24d8ea939f2e4
unknown <jonesa@Jonesa.cybage.com>
2015-05-05 10:22:31 +0530

    3 change

--- src/main/java/artifa/App.java
+++ src/main/java/artifa/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
 	System.out.println( "This is just an change Hello World!" );
+	System.out.println( "This is just an 2 change Hello World!" );
     }
 }
